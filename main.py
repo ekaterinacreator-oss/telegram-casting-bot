50505f62fc922b964d0f2ac250226957e8128c42
ekaterinacreator-oss <ekaterina.creator@gmail.com>
2026-02-15 07:27:09 +0000

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from aiogram import Bot, Dispatcher
 
 # ==== 1. НАСТРОЙКИ ====
 # Эти данные ты получишь на сайте my.telegram.org
-API_ID = int("ВАШ_API_ID")
-API_HASH = "ВАШ_API_HASH"
+API_ID = int("29333521")
+API_HASH = "a628993ea695e1580829863dac9d99b9"
 
 # Твой bot token от BotFather:
 BOT_TOKEN = "8485398329:AAFN7xq37IAuh-mvdzC_XrYkdw_ICxGyYBc"
@@ -16,52 +16,52 @@ USER_ID = int("456496188")
 
 # Список каналов, которые бот должен отслеживать (их @имена или numeric ID):
 CHANNELS_TO_WATCH = [
-    "‎https://t.me/PremierAgency24"‎
-"https://t.me/castingnncway‎"‎
-"‎https://t.me/spotcasting"‎
-"‎https://t.me/c/2090693689"‎
-"‎https://t.me/c/2569890697"‎
-"‎https://t.me/castings5000"‎
-"‎https://t.me/KastingEA"‎
-"‎https://t.me/+-8BL9dl926I5ZWYy"‎
-"https://t.me/vsemassovki‎"‎
-"‎https://t.me/c/3046014819/996"‎
-"‎https://t.me/castingsertainss"‎
-"https://t.me/modeli_kastingi_msk‎"‎
-"‎https://t.me/mskeventjob"‎
-"https://t.me/castingsactors‎"‎
-"‎https://t.me/facetoface_ma"‎
-"‎https://t.me/c/1797191303"‎
-"‎https://t.me/cast_day"‎
-"https://t.me/castingsmsc‎"‎
-"‎https://t.me/tfp_commerce"‎
-"https://t.me/newcast2023‎"‎
-"‎https://t.me/casting_moscoww"‎
-"‎https://t.me/artiyork_casting_director"‎
-"‎https://t.me/KatyaLetoKastDir"‎
-"‎https://t.me/moscowfilms"‎
-"https://t.me/kinoandreclama‎"‎
-"https://t.me/SPBmskcasting‎"‎
-"‎https://t.me/acmodasi_ru"‎
-"https://t.me/kastingi2‎"‎
-"‎https://t.me/artpablopirat"‎
-"‎https://t.me/kastingi3"‎
-"‎https://t.me/Agent_golomidov"‎
-"‎https://t.me/AgencyMalykh"‎
-"‎https://t.me/topmediarina"‎
-"https://t.me/elimodmodels‎"‎
-"‎https://t.me/gogotovacasting"‎
-"https://t.me/c/1406211267‎"‎
-"‎https://t.me/c/2578843731"‎
-"‎https://t.me/castingpolinagroup"‎
-"‎https://t.me/kastingiD"‎
-"‎https://t.me/SilaModelingaVIDTV"‎
-"https://t.me/TALOCHKiNAvera_official‎"‎
-"‎https://t.me/vbmodel"
-"‎https://t.me/fancypeolechannel"‎
-"‎https://t.me/newfacescasting"‎
-"‎https://t.me/nebytov_entr"‎
-"https://t.me/c/2996032086‎"‎
+    "https://t.me/PremierAgency24",
+    "https://t.me/castingnncway",
+    "https://t.me/spotcasting",
+    "https://t.me/c/2090693689",
+    "https://t.me/c/2569890697",
+    "https://t.me/castings5000",
+    "https://t.me/KastingEA",
+    "https://t.me/joinchat/-8BL9dl926I5ZWYy",
+    "https://t.me/vsemassovki",
+    "https://t.me/c/3046014819",
+    "https://t.me/castingsertainss",
+    "https://t.me/modeli_kastingi_msk",
+    "https://t.me/mskeventjob",
+    "https://t.me/castingsactors",
+    "https://t.me/facetoface_ma",
+    "https://t.me/c/1797191303",
+    "https://t.me/cast_day",
+    "https://t.me/castingsmsc",
+    "https://t.me/tfp_commerce",
+    "https://t.me/newcast2023",
+    "https://t.me/casting_moscoww",
+    "https://t.me/artiyork_casting_director",
+    "https://t.me/KatyaLetoKastDir",
+    "https://t.me/moscowfilms",
+    "https://t.me/kinoandreclama",
+    "https://t.me/SPBmskcasting",
+    "https://t.me/acmodasi_ru",
+    "https://t.me/kastingi2",
+    "https://t.me/artpablopirat",
+    "https://t.me/kastingi3",
+    "https://t.me/Agent_golomidov",
+    "https://t.me/AgencyMalykh",
+    "https://t.me/topmediarina",
+    "https://t.me/elimodmodels",
+    "https://t.me/gogotovacasting",
+    "https://t.me/c/1406211267",
+    "https://t.me/c/2578843731",
+    "https://t.me/castingpolinagroup",
+    "https://t.me/kastingiD",
+    "https://t.me/SilaModelingaVIDTV",
+    "https://t.me/TALOCHKiNAvera_official",
+    "https://t.me/vbmodel",
+    "https://t.me/fancypeolechannel",
+    "https://t.me/newfacescasting",
+    "https://t.me/nebytov_entr",
+    "https://t.me/c/2996032086"
 ]
 
 # ==== 2. Личная информация и критерии ====
@@ -86,7 +86,7 @@ WAIST_RANGE = (WAIST - 3, WAIST + 3)
 HIPS_RANGE = (HIPS - 3, HIPS + 3)
 
 AGE_MIN = 27
-AGE_MAX = 35
+AGE_MAX = 37
 
 CITY = "моск"  # ищем по ключу “моск” → Москва
 
